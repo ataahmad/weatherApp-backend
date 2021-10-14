e0abd3ff5b33b4bf53deac6d1e8564c27f73ff09
Ata Ahmad <ataahmad@Atas-MBP.lan>
2021-10-14 08:59:10 -0700

    Testing out CI integration

--- test/index.js
+++ test/index.js
@@ -5,49 +5,60 @@ const chaiHttp = require("chai-http");
 const server = require("../index");
 
 
-let should = chai.should();
+const should = chai.should();
 
 
 chai.use(chaiHttp);
 
 
 describe('Server', () => {
-    // beforeEach((done) => { //Before each test we empty the database
-    //     Book.remove({}, (err) => {
-    //        done();
-    //     });
-    // });
-/*
-  * Test the /GET route
-  */
-  describe('/GET', () => {
-      it('Server Get Route is working correctly', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((err, res) => {
-                  res.should.have.status(200);
-              done();
-            });
-      });
-  });
 
-  describe('/POST', () => {
-      it('Server Post Route is working correctly', (done) => {
-          chai.request(server)
-            .post('/')
-            .type('form')
-            .set('content-type', 'application/x-www-form-urlencoded')
-            .send({
-                address1: "1669 Thomas Avenue",
-                address2: "",
-                city: "San Diego",
-                state: "CA"
-            })
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
-      });
-  });
+    describe('/GET', () => {
+        it('Server Get Route is working correctly', (done) => {
+            chai.request(server)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
 
+    describe('/POST', () => {
+        it('Server Post Route is working correctly', (done) => {
+            chai.request(server)
+                .post('/')
+                .type('form')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send({
+                    address1: "1669 Thomas Avenue",
+                    address2: "",
+                    city: "San Diego",
+                    state: "CA"
+                })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+
+    describe('/POST', () => {
+        it('Server Post Route is working correctly', (done) => {
+            chai.request(server)
+                .post('/')
+                .type('form')
+                .set('content-type', 'application/x-www-form-urlencoded')
+                .send({
+                    address1: "3863 Ingraham Street",
+                    address2: "",
+                    city: "San Diego",
+                    state: "CA"
+                })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
 });
